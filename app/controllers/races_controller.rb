54b30247ea58d8543fb43a1ddf7ca95bad320b44
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-16 08:03:24 -0400

    update race controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,12 +2,13 @@ class RacesController < ApplicationController
 
     def index
         races = Race.all
+        #instance var not needed because we're not rendering in views
         render json: races
     end
 
     def show
         race = Race.find(params[:id])
-        render json: book
+        render json: race
     end
-    
+
 end
